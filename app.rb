fb11abce43d7958c03b461020172f9aa09842fc1
thomasfrl <feral.thomas@gmail.com>
2019-01-22 18:49:48 +0100

    mise à jour

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ $LOAD_PATH.unshift(File.expand_path('./../lib', __FILE__))
 require 'app/townHall.rb'
 
 list_email =  TownHall.new
+#créé une instance de la classe TownHall et lui applique des methodes
 list_email.save_as_JSON
 list_email.save_as_csv
 list_email.save_as_spreadsheet
--- lib/app/townHall.rb
+++ lib/app/townHall.rb
@@ -4,11 +4,13 @@ class TownHall
 	attr_reader :hash_townhall
 
 	def get_townhall_email(townhall_url)
+		#renvoi l'adresse email trouvé sur le site mis en argument
 		page = Nokogiri::HTML(open(townhall_url))   
 		page.xpath('//div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
 	end
 
 	def get_townhall_urls
+	#renvoi un array contenant toutes les url des sites des villes du val d'oise et un array contenant leurs noms
 		town_name =[]
 		town_url = []
 		page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))   
@@ -21,18 +23,21 @@ class TownHall
 
 
 	def initialize
-=begin
+		#initialise l'attribu @hash_townhall avec un Hash contenant en key les noms des villes et en value les adresses email
 		townhall_info = get_townhall_urls
 		url_list = townhall_info[0]
 		name_list = townhall_info[1]
+		@hash_townhall = {}
 
 		url_list.each_with_index do |townhall_url,i|
 			@hash_townhall[name_list[i]] = get_townhall_email(townhall_url)
 		end
-=end
+		#si le site est inaccessible demain, mettre en commentaire les lignes ci-dessus et aciver les lignes ci-dessous
+=begin		
 		File.open("db/copie.JSON").each do |line|
 			@hash_townhall = JSON.parse(line)
 		end
+=end
 
 	end
 
@@ -41,17 +46,23 @@ class TownHall
 	end
 
 	def save_as_JSON
+	#enregistre l'instance de classe en format Json
 		File.open("db/email.JSON","w+") do |line|
 		  line.write(@hash_townhall.to_json)
 		end
 	end
 
-	def save_as_csv
+	def array_townhall
+		#retourne un array à partir de l'attribu hash_townhall
 		array_townhall = []
 		@hash_townhall.each do |name_town,email_town| 
 			array_townhall << [name_town, email_town]
 		end
+		return array_townhall
+	end
 
+	def save_as_csv
+	#enregistre l'instance de classe en format csv
 		CSV.open("db/email.csv","w+") do |csv|
 			csv << ["city","email"]
 		  array_townhall.each do |a|
@@ -61,11 +72,7 @@ class TownHall
 	end
 
 	def save_as_spreadsheet
-		array_townhall = []
-		@hash_townhall.each do |name_town,email_town| 
-			array_townhall << [name_town, email_town]
-		end
-
+	#enregistre l'instance de classe en spreadsheet sur google-drive
 		session = GoogleDrive::Session.from_config("config.JSON")
 		spread_sheet = session.create_spreadsheet("liste email des mairies")
 		ws = spread_sheet.worksheets[0]
